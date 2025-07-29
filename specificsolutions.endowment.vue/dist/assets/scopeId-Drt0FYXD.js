ebfc01c2dddc5093fc879404e7120305de11b7c8
خالد النفاتي <tajura.libya@gmail.com>
2025-07-29 22:37:40 +0200

    Build

---
+++ specificsolutions.endowment.vue/dist/assets/scopeId-Drt0FYXD.js
@@ -0,0 +1,1 @@
+import{bL as o}from"./index-t_jZ6uLN.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
---
+++ specificsolutions.endowment.vue/dist/assets/useAbility-B4WBxTpO.js
@@ -0,0 +1,1 @@
+import{cf as i}from"./index-t_jZ6uLN.js";const t=()=>i();export{t as u};
